a5b11636622f3fb4bda6dcb4f172ea5397cafb8a
f22212225 <f22212225@gmail.com>
2026-06-22 17:59:52 +0800

    自動設定測試環境變數

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.test.js'],
   clearMocks: true,
   collectCoverageFrom: ['src/**/*.js', '!src/server.js'],
+  setupFiles: ['./src/__tests__/setup.js'],
 };
