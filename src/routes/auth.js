bc001dabb4845078323b9cad9ee0ea7a417ea800
f22212225 <f22212225@gmail.com>
2026-06-22 18:09:05 +0800

    Update auth routes: add /me endpoint with authentication middleware

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { signup, login } = require('../controllers/authController');
+const { signup, login, me } = require('../controllers/authController');
+const authenticate = require('../middleware/authenticate');
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/me', authenticate, me);
 
 module.exports = router;
