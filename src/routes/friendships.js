a9b545c19bad13e66f0987436860a568fa2b9591
yu <271432374+yuaangela@users.noreply.github.com>
2026-07-09 15:13:21 +0800

    feat: 新增軟刪除好友功能 API

--- src/routes/friendships.js
+++ src/routes/friendships.js
@@ -5,11 +5,13 @@ import {
   rejectFriendship,
   requestFriendship,
 } from "../controllers/friendshipController.js";
+import { removeFriendship } from "../controllers/friendshipController.js";
 
 const router = express.Router();
 
 router.post("/request", authenticate, requestFriendship);
 router.post("/:id/accept", authenticate, acceptFriendship);
 router.post("/:id/reject", authenticate, rejectFriendship);
+router.delete("/:id", authenticate, removeFriendship);
 
 export default router;
